6282b53d3d30cc040b060abf1ccec47cae8478b2
DINH VO BAO CHAU <chaudinh@DINHs-MBP.net.fpt>
2019-04-05 01:51:22 +0700

    Create Recipe Details component, Add Recipe Details component

--- src/App.js
+++ src/App.js
@@ -8,28 +8,35 @@ import './App.css';
 
 class App extends Component {
   state = {
-    recipes: [],
-    url: "https://www.food2fork.com/api/search?key=a7f36b988578bb2cf1aaf95f8f3db9c1"
+    recipes: recipes,
+    url: "https://www.food2fork.com/api/search?key=a7f36b988578bb2cf1aaf95f8f3db9c1",
+    details_id: 35382
   };
 
-  async getRecipes() {
-    try {
-      const data = await fetch(this.state.url);
-      const jsonData = await data.json();
-      this.setState({
-        recipes: jsonData.recipes
-      })
-    }
-    catch(error) {
-      console.log(error);
-    }
-  }
+  // async getRecipes() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     })
+  //   }
+  //   catch(error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
 
   render() {
+    // console.log(this.state.recipes);
+
     return (
       <React.Fragment>
-        <RecipeList />
-        <RecipeDetails />
+        {/* <RecipeList recipes={this.state.recipes} /> */}
+        <RecipeDetails id={this.state.details_id} />
       </React.Fragment>
     );
   }
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,10 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import { recipe } from "../tempDetails";
 
 export class RecipeDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: recipe,
+      url: `https://www.food2fork.com/api/get?key=a7f36b988578bb2cf1aaf95f8f3db9c1&rId=${this.props.id}`
+    }
+  }
   render() {
+    const {
+      image_url,
+      publisher,
+      publisher_url,
+      source_url,
+      title,
+      ingredients
+    } = this.state.recipe;
+    
     return (
       <React.Fragment>
-        <h1>Hello from  Recipe Details</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 my-3">
+              <button type="button" className="btn btn-warning mb-5 text-capitalize">back to recipe list</button>
+              <img src={image_url} className="d-block w-100" alt="recipe" />
+            </div>
+            <div className="col-10 mx-auto col-md-6 my-3">
+              <h6 className="uppercase">{title}</h6>
+              <h6 className="text-warning text-capitalize text-slanted">provided by {publisher}</h6>
+              <a href={publisher_url} className="btn btn-primary mt-2 text-capitalize" target="_blank" rel="noopener noreferrer">publisher webpage</a>
+              <a href={source_url} className="btn btn-success mx-3 mt-2 text-capitalize" target="_blank" rel="noopener noreferrer">recipe url</a>
+              <ul className="list-group mt-4">
+                <h2 className="mt-3 mb-4">Ingredients</h2>
+                {
+                  ingredients.map((ingredient, index) => {
+                    return (
+                      <li key={index} className="list-group-item text-slanted">
+                        {ingredient}
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     )
   }
